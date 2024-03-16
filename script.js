83949ade0252568203a4d9475041beab845ff45f
zeenat <zeenu.23@gmail.com>
2024-03-16 16:06:12 +0530

    Added a fake hacking program that uses fetch API to send a POST request to a fake endpoint and prints the response.

--- script.js
+++ script.js
@@ -8,3 +8,48 @@ setInterval(() => {
     let d = date.toLocaleTimeString('en-US', options)
     time.innerHTML = d   
 }, 1000);
+
+/* Write a JS program to pretend to look like a hacker. Write an async function which will simply display the following output
+
+Initializing Hack program...
+Hacking Ashish username...
+Username found ashish17...
+Connecting to facebook...
+
+Try to use HTML and styling if possible */
+
+
+//Sending POST request with Fetch API
+
+const createTodo = async(todo) => {
+    let options = {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json'},
+        body: JSON.stringify(todo),
+    }
+    let res = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+    let data = await res.json()
+    return data;
+}
+
+const getTodo = async (id) => {
+    let response = fetch('https://jsonplaceholder.typicode.com/posts/1')
+    let r = await response.json()
+        
+  .then((json) => console.log(json));
+}
+
+const mainFunc = async () => {
+    let todo = {
+        title: 'Zeenat',
+        body: 'Abedin',
+        userId: 1,
+        }
+    let todoResponse = await createTodo(todo)
+    console.log(todoResponse)
+}
+
+mainFunc()
+
+
+  
